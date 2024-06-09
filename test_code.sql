757b4824417d901f173fd29948879450453e56da
St4rLighr <102866859+St4rLighr@users.noreply.github.com>
2024-06-09 18:09:29 +0100

    Create test_code.sql

---
+++ test_code.sql
@@ -0,0 +1,32 @@
+```sql
+  
+/*
+Created by: Simi Ibraheem
+Create Date: 05/05/2024
+Description: This query displays all customers first, last names and email addresses
+*/
+
+-- used to write comments 
+
+OR
+
+/*
+For longer comments
+
+*/
+
+SELECT 
+	FirstName  AS [Customer First Name], 
+	LastName AS 'Customer Last Name', 
+	Email AS EMAIL
+
+FROM 
+	Customer
+
+ORDER BY 
+	FirstName ASC, -- This column is now sorted in asc order 
+	LastName DESC -- This column is now sorted in desc order 
+	
+LIMIT 10 -- used to get the top 10 rows 
+  
+```
